e17e904daa2eee869567095b33d72fd218561b52
sexroute <zhaodali78@gmail.com>
2013-02-27 18:57:03 +0800

    [CHG] 测试提交

--- testOpt.m
+++ testOpt.m
@@ -6,6 +6,6 @@ G_Param(4)=1;
 G_Param(5)=120e-9;  %F 法拉 
 G_Param(6)=6.8e-9;  %F 法拉
 G_Param(7)=18e-9;   %F 法拉
-G_Param(8)=1;
+G_Param(8)=1*1;
 G_Param(9)=0.0204*1.00011*1.0108;
 computeIntRMS(G_Param);

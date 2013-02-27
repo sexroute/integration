5df69a6d1bcc3dec81510290681bafa9b7bc9f60
sexroute <zhaodali78@gmail.com>
2013-02-27 18:54:14 +0800

    [NEW] 数值积分优化

---
+++ testOpt.m
@@ -0,0 +1,11 @@
+G_Param = [];
+G_Param(1) =150e3;   % Ω 欧姆
+G_Param(2)=1.5e6;   % Ω 欧姆
+G_Param(3)=1.5e6;      % Ω 欧姆
+G_Param(4)=1;
+G_Param(5)=120e-9;  %F 法拉 
+G_Param(6)=6.8e-9;  %F 法拉
+G_Param(7)=18e-9;   %F 法拉
+G_Param(8)=1;
+G_Param(9)=0.0204*1.00011*1.0108;
+computeIntRMS(G_Param);
